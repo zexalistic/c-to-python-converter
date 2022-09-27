c714a40c2e266d71306c702180ae6cfc57842cea
zexalistic <to.gnu.is.not.unix@gmail.com>
2022-09-27 23:43:43 +0800

    version 2.1.0; Add GUI

--- myGUI.py
+++ myGUI.py
@@ -1,14 +1,31 @@
+"""
+    @usage: automatically generate Cpython wrapper file according to header files
+    @date: 2021-03-05
+    @author: Yihao Liu
+    @email: lyihao@marvell.com
+    @python: 3.7
+    @latest modification: 2022-09-27
+    @version: 2.1.0
+    @update: add GUI
+"""
 import PySimpleGUI as sg
-import os
-import shutil
 import json
+from parse import Parser
 
-header_files = []
+
+def import_json(config_json:str, config:dict):
+    with open(config_json, 'r') as fp:
+        env = json.load(fp)
+    config["exception_dict"].update(env.get('exception_dict', dict()))
+    config["header_files"].extend(env.get('header_files', list()))
+    config["predefined_macro_dict"].update(env.get('predefined_macro_dict', dict()))
+    config["project_folders"].extend(env.get('project_folders', list()))
+    config["is_multiple_file"] = env.get('is_multiple_file', False)
 
 
 def make_window(theme):
     sg.theme(theme)
-    menu_def = [['&File', ['&Import settings from config.json', '&Exit']],
+    menu_def = [['&File', ['&Import settings from config.json', '&Export settings as config.json', '&Exit']],
                 ['&Help', ['&About', '&user guide']]]
     right_click_menu_def = [[], ['Versions', 'Exit']]
 
@@ -16,30 +33,24 @@ def make_window(theme):
         # [sg.Checkbox('split the output into multiple files', default=False, k='-CB-')],
         # [sg.Combo(values=('Combo 1', 'Combo 2', 'Combo 3'), default_value='Combo 1', readonly=False, k='-COMBO-'),
         #  sg.OptionMenu(values=('Option 1', 'Option 2', 'Option 3'), k='-OPTION MENU-'), ],
-        [sg.Text("Files to convert")],
-        [sg.Listbox(values=header_files,
-                    size=(20, 10),
-                    key='-header files-',
+        [sg.Text("Files and folders to convert")],
+        [sg.Listbox(values=config["header_files"] + config["project_folders"],
+                    size=(20, 5),
+                    key='-files and folders-',
                     expand_x=True,
                     enable_events=True)],
-         [sg.Button("Add Folder"), sg.Button("Add File"), sg.Button("Delete")],
+         [sg.Button("Add Folder"), sg.Button("Add File"), sg.Button("Delete"), sg.Button("Clear")],
          [sg.ProgressBar(100, orientation='h', size=(20, 20), key='-PROGRESS BAR-'), sg.Button('Convert')]]
-    logging_layout = [[sg.Text("Anything printed will display here!")],
-                      [sg.Multiline(size=(60, 15), font='Courier 8', expand_x=True, expand_y=True, write_only=True,
-                                    reroute_stdout=True, reroute_stderr=True, echo_stdout_stderr=True, autoscroll=True,
-                                    auto_refresh=True)]
-                      ]
-    theme_layout = [[sg.Listbox(values=sg.theme_list(),
-                                size=(20, 12),
-                                key='-THEME LISTBOX-',
-                                enable_events=True)],
-                    [sg.Button("Set Theme")]]
+    # logging_layout = [[sg.Text("Debug info")],
+    #                   [sg.Multiline(size=(60, 15), font='Courier 8', expand_x=True, expand_y=True, write_only=True,
+    #                                 reroute_stdout=True, reroute_stderr=True, echo_stdout_stderr=True, autoscroll=True,
+    #                                 auto_refresh=True)]
+    #                   ]
 
-    layout = [[sg.MenubarCustom(menu_def, key='-MENU-', font='Courier 15', tearoff=True)]]
+    layout = [[sg.MenubarCustom(menu_def, key='-MENU-', font='Courier 10', tearoff=True)]]
     layout += [[sg.TabGroup([[sg.Tab('Basic', settings_layout),
-                              sg.Tab('Theming', theme_layout),
-                              sg.Tab('Debug log', logging_layout)]], key='-TAB GROUP-', expand_x=True, expand_y=True),
-
+                              # sg.Tab('Debug log', logging_layout)
+                              ]], key='-TAB GROUP-', expand_x=True, expand_y=True)
                 ]]
     layout[-1].append(sg.Sizegrip())
     window = sg.Window('C to python wrapper', layout, right_click_menu=right_click_menu_def,
@@ -66,45 +77,59 @@ txt_user_guide = "1.Add files or folders to convert\n" \
                  "2. Click Convert Button\n" \
                  "3. Check the log and results in the output folder\n"
 
+if __name__ == '__main__':
+    config = dict()
+    with open('config.json', 'r') as fp:    # load previous configuration
+        config = json.load(fp)
 
-window = make_window(sg.theme("BluePurple"))
+    window = make_window(sg.theme("TealMono"))
+    # This is an Event Loop
+    while True:
+        event, values = window.read(timeout=100)
+        if event in (None, 'Exit'):
+            break
+        elif event == 'About':
+            sg.popup(txt_about, keep_on_top=True)
+        elif event == 'Import settings from config.json':
+            config_json = sg.popup_get_file('Import settings from config.json', keep_on_top=True)
+            import_json(config_json, config)
+        elif event == 'Export settings as config.json':
+            config_json = sg.popup_get_file('Export settings as config.json', keep_on_top=True)
+            with open(config_json, 'w') as fp:
+                json.dump(config, fp, indent=4)
+            sg.popup("Export successfully.", keep_on_top=True)
+        elif event == 'user guide':
+            sg.popup(txt_user_guide, keep_on_top=True)
+        elif event == 'Convert':
+            progress_bar = window['-PROGRESS BAR-']
+            for i in range(100):
+                progress_bar.update(current_count=i + 1)
+            parser = Parser()
+            parser.env = config
+            parser()
+            sg.popup("Please check debug.log.", keep_on_top=True)
+        elif event == "Add Folder":
+            folder = sg.popup_get_folder('Choose your folder', keep_on_top=True)
+            config["project_folders"].append(folder)
+            window['-files and folders-'].update(config["header_files"] + config["project_folders"])
+        elif event == "Add File":
+            files = sg.popup_get_file('Choose your file', keep_on_top=True)
+            config["header_files"].append(files)
+            window['-files and folders-'].update(config["header_files"] + config["project_folders"])
+        elif event == "Delete":
+            item_chosen = values['-files and folders-'][0]
+            if item_chosen in config["header_files"]:
+                config["header_files"].remove(item_chosen)
+            else:
+                config["project_folders"].remove(item_chosen)
+            window['-files and folders-'].update(config["header_files"] + config["project_folders"])
+        elif event == "Clear":
+            config["header_files"] = list()
+            config["project_folders"] = list()
+            window['-files and folders-'].update(config["header_files"] + config["project_folders"])
+        elif event == 'Versions':
+            sg.popup_scrolled(f'Version=2.0.12', keep_on_top=True, non_blocking=True)
 
-# This is an Event Loop
-while True:
-    event, values = window.read(timeout=100)
-    if event in (None, 'Exit'):
-        break
-    elif event == 'About':
-        sg.popup(txt_about, keep_on_top=True)
-    elif event == 'Import settings from config.json':
-        config_json = sg.popup_get_file('Choose your file', keep_on_top=True)
-        shutil.copy(src=config_json, dst='config.json')
-    elif event == 'user guide':
-        sg.popup(txt_user_guide, keep_on_top=True)
-    elif event == 'Convert':
-        progress_bar = window['-PROGRESS BAR-']
-        for i in range(100):
-            progress_bar.update(current_count=i + 1)
-        sg.popup("Conversion done.", keep_on_top=True)
-    elif event == "Add Folder":
-        folder = sg.popup_get_folder('Choose your folder', keep_on_top=True)
-        header_files.append(folder)
-        window['-header files-'].update(header_files)
-    elif event == "Add File":
-        files = sg.popup_get_file('Choose your file', keep_on_top=True)
-        header_files.append(files)
-        window['-header files-'].update(header_files)
-    elif event == "Delete":
-        item_chosen = values['-header files-'][0]
-        header_files.remove(item_chosen)
-        window['-header files-'].update(header_files)
-    elif event == "Set Theme":
-        theme_chosen = values['-header files-'][0]
-        window.close()
-        window = make_window(theme_chosen)
-    elif event == 'Versions':
-        sg.popup_scrolled(f'Version=2.0.12', keep_on_top=True, non_blocking=True)
-
-window.close()
-exit(0)
+    window.close()
+    exit(0)
 
