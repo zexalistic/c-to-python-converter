345c72efb17f81a601b78223a2dca5858ec7f75c
Yihao Liu <to.gnu.is.not.unix@gmail.com>
2023-06-02 14:35:35 -0700

    add testcase

---
+++ testcases.py
@@ -0,0 +1,20 @@
+"""
+    @usage: testcase template
+"""
+import os
+import logging
+import time
+import traceback
+from output.enum_class import *
+from output.structure_class import *
+from output.python_API import *
+
+if __name__ == "__main__":
+    logging.info("Function name : hello_world")
+    something = MY_BOOL.MY_TRUE.value
+    try:
+        hello_world(something)
+    except Exception:
+        traceback.print_exc()
+    logging.debug(f"something = {something}")
+    logging.info("\n")
